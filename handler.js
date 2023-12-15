f88ae41da42297fc92e13cb3ea18803539d1bd79
wdnjnnh <c264bsx3465@bangkit.academy>
2023-12-15 10:33:31 +0700

    registration edit + geolocation + user data console

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, serverTimestamp, doc, getDocs  } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getFirestore, collection, setDoc, serverTimestamp, doc, getDoc, GeoPoint  } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
 
 // Firebase configuration
@@ -30,33 +30,37 @@ auth.onAuthStateChanged(user =>{
   }
 })
 
-// Registrasi
+// Registration
 const registerForm = document.querySelector('#registerForm');
 registerForm.addEventListener('submit', async (e) => {
 
-  // Dapatkan informasi pengguna
+  // Get user input
   const email = registerForm['reg-email'].value;
   const password = registerForm['reg-password'].value;
   const username = registerForm['reg-username'].value;
   const gender = registerForm['reg-gender'].value;
+  const userContact = registerForm['reg-contact'].value
   const userBirthdate = new Date(registerForm['reg-birthdate'].value);
   e.preventDefault();
   try {
-    // Buat pengguna di Otentikasi Firebase
+    // Create user in firebase auth
     const cred = await createUserWithEmailAndPassword(auth, email, password);
+    
+    //set uid as document id in Firestore
+    const userRef = doc(db, "users", cred.user.uid)
 
-    // Simpan data pengguna tambahan ke Firestore
-    await addDoc(collection(db, "users"), {
+    // Save user data to Firestore
+    await setDoc(userRef, {
       uid: cred.user.uid,
       email,
       username,
       gender,
       createdAt: serverTimestamp(),
-      contact: "",
+      contact: userContact,
       birthdate: userBirthdate,
       geopoint: null,
       isMale: gender === "male", // Atur isMale berdasarkan nilai gender
-      name: ""
+      name: username,
     });
 
   } catch (error) {
@@ -76,45 +80,67 @@ registerForm.addEventListener('submit', async (e) => {
       const loginForm = document.querySelector('#loginForm');
       loginForm.addEventListener('submit', async (e) => {
           e.preventDefault(); 
-  
+
           //get user info
           const email = loginForm['login-email'].value;
           const password = loginForm['login-password'].value;
           await signInWithEmailAndPassword(auth, email, password);
-          
-          });
-    
-    //TODO get user data
-/*const userDataLink = document.querySelector('#userLink');
-userDataLink.addEventListener('click', async (e) => {
-  e.preventDefault();
-
-  auth.onAuthStateChanged(async (user) => {
-    if (user) {
-      const userID = user.uid;
-
-      try {
-        const userRef = doc(db, "users", userID);
-        const userDoc = await getDoc(userRef);
-
-        if (userDoc.exists()) {
-          const userData = userDoc.data();
-
-          // Display user data on the page
-          document.getElementById("userEmail").textContent = userData.email;
-          document.getElementById("userName").textContent = userData.name;
-          document.getElementById("userBirthdate").textContent = userData.birthdate ? new Date(userData.birthdate.seconds * 1000).toLocaleDateString() : "";
-          document.getElementById("userContact").textContent = userData.contact;
-          document.getElementById("userCreatedAt").textContent = userData.createdAt ? new Date(userData.createdAt.seconds * 1000).toLocaleString() : "";
-          document.getElementById("userIsMale").textContent = userData.isMale ? "Yes" : "No";
+          //default long lat
+          let latitude = 0
+          let longitude = 0
+          if (navigator.permissions) {
+            navigator.permissions.query({ name: 'geolocation' }).then((result) => {
+                if (result.state === 'granted') {
+                    // Izin diberikan, Anda dapat menggunakan geolokasi
+                    navigator.geolocation.getCurrentPosition((position) => {
+                        const latitude = position.coords.latitude;
+                        const longitude = position.coords.longitude;
+                        console.log("Location:", latitude, longitude);
+                    });
+                } else {
+                    // Izin tidak diberikan
+                    console.log("Location:", latitude, longitude);
+                }
+            }).catch((error) => {
+                console.error("Error checking geolocation permission:", error);
+            });
         } else {
-          console.log("User document not found");
+            // Browser tidak mendukung navigator.permissions
+            console.log("Geolocation not supported");
         }
-      } catch (error) {
-        console.error("Error getting user document: ", error);
-      }
-    } else {
-      console.log("User logged out.")
-    }
-  });
-});*/
+        
+          // get user data test
+
+        auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                try {
+                    const userRef = doc(db, "users", user.uid);
+                    const userDoc = await getDoc(userRef);
+    
+                    if (userDoc.exists()) {
+                        const userData = userDoc.data();
+    
+                        // Display user data in the popup
+                        console.log(userData.email);
+                        console.log(userData.name);
+                        console.log(userData.birthdate ? new Date(userData.birthdate.seconds * 1000).toLocaleDateString() : "");
+                        console.log(userData.contact);
+                        console.log(userData.createdAt ? new Date(userData.createdAt.seconds * 1000).toLocaleString() : "");
+                        if (userData.isMale){
+                          console.log("Gender: Male")
+                        }else{
+                          console.log("Gender: Female")
+                        }
+                    } else {
+                        console.log("User document not found");
+                    }
+                } catch (error) {
+                    console.error("Error getting user document: ", error);
+                }
+            } else {
+                console.log("User logged out.")
+            }
+        });
+    });
+    
+    
